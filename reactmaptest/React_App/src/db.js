08357c9e715a5af627186dc75b49585a470693d8
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-12-14 16:38:30 +0100

    minor changes

--- reactmaptest/React_App/src/db.js
+++ reactmaptest/React_App/src/db.js
@@ -1,6 +1,9 @@
 import Dexie from 'dexie';
 
 const db = new Dexie('stgo');
-db.version(1).stores({ pois: '++id , lat , lon , desc' });
+db.version(1).stores({ 
+	pois1: '++id , lat , lon , desc',
+	pois2: '++id , lat , lon , desc'
+});
 
 export default db;
