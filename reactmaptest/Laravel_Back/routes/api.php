b95e44fead16a6bf0432b8ab1bce12891e919342
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-12-14 16:35:46 +0100

    add a test route to send entire in JSON , and DexieController

--- reactmaptest/Laravel_Back/routes/api.php
+++ reactmaptest/Laravel_Back/routes/api.php
@@ -17,6 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/dexie','DexieController@index');
+
 Route::get('/pois','PoiController@index');
 
 Route::post('/answer','PoiController@testData');
