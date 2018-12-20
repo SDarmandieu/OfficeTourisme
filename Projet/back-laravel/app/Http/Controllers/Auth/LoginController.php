7d279496fa1afe87246ece229fa028c585f7bc93
Simon <simon.darmandieu@gmail.com>
2018-12-20 16:47:50 +0100

    minor changes

--- Projet/back-laravel/app/Http/Controllers/Auth/LoginController.php
+++ Projet/back-laravel/app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/city';
 
     /**
      * Create a new controller instance.
