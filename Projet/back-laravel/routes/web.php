9bc2b56bcc3df9ecaac3cb6a0d2e26dce907e8c4
Simon <simon.darmandieu@gmail.com>
2018-12-21 17:00:23 +0100

    city routes

--- Projet/back-laravel/routes/web.php
+++ Projet/back-laravel/routes/web.php
@@ -10,15 +10,21 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/','HomeController@index')->name('home');
 
-Route::get('/', function () {
-    return view('auth.login');
-});
+Route::view('/login','auth.login');
 
 Auth::routes();
 
+/**
+ * City crud routes
+ */
 Route::get('/city', 'CityController@index')->name('cityIndex')->middleware('auth');
-Route::get('/city/create' , 'CityController@create')->name('cityCreate')->middleware('auth');
 
+Route::view('/city/create' , 'city.create')->name('cityCreate')->middleware('auth');
 Route::post('/city/store' , 'CityController@store')->name('cityStore')->middleware('auth');
-Route::get('/city/destroy/{id}' , 'CityController@destroy')->name('cityDestroy')->middleware('auth');
+
+Route::delete('/city/destroy/{id}' , 'CityController@destroy')->name('cityDestroy')->middleware('auth');
+
+Route::get('/city/edit/{id}','CityController@edit')->name('cityEdit')->middleware('auth');
+Route::put('/city/update/{id}','CityController@update')->name('cityUpdate')->middleware('auth');
