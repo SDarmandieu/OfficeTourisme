6176566883149c97115922cfbbbe7f0044e26491
Simon <simon.darmandieu@gmail.com>
2019-01-02 10:55:58 +0100

    static city factory

---
+++ Projet/back-laravel/database/factories/CityFactory.php
@@ -0,0 +1,11 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\City::class, function (Faker $faker) {
+    return [
+        'name' => 'foo',
+        'lat' => rand(1,50),
+        'lon' => rand(1,50)
+    ];
+});
