2cbadd1b9ccc6b922b0870435585a5e9455540fb
Simon <simon.darmandieu@gmail.com>
2019-01-08 14:48:33 +0100

    fix

--- reactmaptest/React_App/src/db.js
+++
@@ -1,9 +0,0 @@
-import Dexie from 'dexie';
-
-const db = new Dexie('stgo');
-db.version(1).stores({ 
-	pois1: '++id , lat , lon , desc',
-	pois2: '++id , lat , lon , desc'
-});
-
-export default db;
