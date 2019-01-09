fee58e0b5709c4ef0aee8abc030a80146cd20215
Simon <simon.darmandieu@gmail.com>
2019-01-09 14:22:17 +0100

    change controller to handle image storage

--- Projet/back-laravel/app/Http/Controllers/ImageController.php
+++ Projet/back-laravel/app/Http/Controllers/ImageController.php
@@ -6,6 +6,7 @@ use App\Image;
 use App\City;
 use App\Imagetype;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -18,6 +19,9 @@ class ImageController extends Controller
     {
         $city = City::find($city_id);
         $images = Image::where('city_id', $city_id)->get();
+        foreach($images as $image){
+            $image->url = Storage::url($image->filename);
+        }
         return view('image.index', compact('images', 'city'));
     }
 
@@ -30,23 +34,28 @@ class ImageController extends Controller
     {
         $city = City::find($city_id);
         $imagetypes = Imagetype::all();
-        return view('image.create', compact('city','imagetypes'));
+        return view('image.create', compact('city', 'imagetypes'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store($city_id,Request $request)
+    public function store($city_id, Request $request)
     {
+        $file = $request->file;
+        $filename = $file->getClientOriginalName();
+
         Image::create([
-            'filename' => $request->input('filename'),
+            'filename' => $filename,
             'alt' => $request->input('alt'),
             'city_id' => $city_id,
             'imagetype_id' => $request->input('type')
         ]);
+
+        Storage::disk('public')->putFileAs('images', $file , $filename , 'public');
 
         return redirect()->route('imageIndex', $city_id)->with('success', 'L\'image a bien été créee.');
     }
@@ -54,7 +63,7 @@ class ImageController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Image  $image
+     * @param  \App\Image $image
      * @return \Illuminate\Http\Response
      */
     public function edit($city_id, $image_id)
@@ -62,38 +71,39 @@ class ImageController extends Controller
         $city = City::findOrFail($city_id);
         $image = Image::findOrFail($image_id);
         $imagetypes = Imagetype::all();
-        return view('image.edit', compact('city','image','imagetypes'));
+        return view('image.edit', compact('city', 'image', 'imagetypes'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Image  $image
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Image $image
      * @return \Illuminate\Http\Response
      */
-    public function update($city_id,$image_id,Request $request)
+    public function update($city_id, $image_id, Request $request)
     {
         $image = Image::findOrFail($image_id);
         $image->update([
-            'filename' => $request->input('filename'),
             'alt' => $request->input('alt'),
             'imagetype_id' => $request->input('type')
         ]);
 
-        return redirect()->route('imageIndex', $city_id)->with('success', 'L\'image a bien été modifieé.');
+        return redirect()->route('imageIndex', $city_id)->with('success', 'L\'image a bien été modifiée.');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Image  $image
+     * @param  \App\Image $image
      * @return \Illuminate\Http\Response
      */
-    public function destroy($city_id,$image_id)
+    public function destroy($city_id, $image_id)
     {
-        Image::find($image_id)->delete();
-        return redirect()->route('imageIndex',$city_id)->with('success', 'L\'image a bien été supprimée.');
+        $image = Image::find($image_id);
+        Storage::disk('public')->delete('images/'.$image->filename);
+        $image->delete();
+        return redirect()->route('imageIndex', $city_id)->with('success', 'L\'image a bien été supprimée.');
 
     }
 }
