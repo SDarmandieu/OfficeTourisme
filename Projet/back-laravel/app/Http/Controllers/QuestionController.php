473333afe7a00a04374b53c38df04a649d4f7a2f
Simon <simon.darmandieu@gmail.com>
2019-01-11 18:22:02 +0100

    question crud

--- Projet/back-laravel/app/Http/Controllers/QuestionController.php
+++ Projet/back-laravel/app/Http/Controllers/QuestionController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Question;
+use App\Point;
+use App\Game;
+use App\City;
+use App\Image;
 use Illuminate\Http\Request;
 
 class QuestionController extends Controller
@@ -22,26 +26,45 @@ class QuestionController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($city_id, $game_id, $point_id)
     {
-        //
+
+        $city = City::findOrFail($city_id);
+        $game = Game::findOrFail($game_id);
+        $point = Point::findOrFail($point_id);
+        $images = Image::where('city_id', '=', $city_id)
+            ->whereHas('imagetype',
+                function ($q) {
+                    $q->where('title', '=', 'game');
+                })->get();
+
+        return view('question.create', compact('city', 'game', 'point', 'images'));
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store($city_id,$game_id,$point_id,Request $request)
     {
-        //
+        $question = Question::create([
+            'content' => $request->input('question'),
+            'expe' => $request->input('expe'),
+            'point_id' => $point_id,
+            'game_id' => $game_id
+        ]);
+
+        $question->images()->attach($request->input('image'));
+
+        return redirect()->route('gamePointIndex',[$city_id,$game_id,$point_id])->with('success', 'La question a bien été créée.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Question  $question
+     * @param  \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function show(Question $question)
@@ -52,7 +75,7 @@ class QuestionController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Question  $question
+     * @param  \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function edit(Question $question)
@@ -63,8 +86,8 @@ class QuestionController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Question  $question
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Question $question)
@@ -75,7 +98,7 @@ class QuestionController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Question  $question
+     * @param  \App\Question $question
      * @return \Illuminate\Http\Response
      */
     public function destroy(Question $question)
