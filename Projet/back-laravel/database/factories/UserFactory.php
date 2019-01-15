a288784b6047ce668d468feb4b6fe122f6335bcf
Simon <simon.darmandieu@gmail.com>
2019-01-15 11:21:42 +0100

    fix user factory

--- Projet/back-laravel/database/factories/UserFactory.php
+++ Projet/back-laravel/database/factories/UserFactory.php
@@ -15,8 +15,8 @@ use App\User;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'name' => "Klint",
-        'email' => "klint@klint.com",
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
