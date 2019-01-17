dc91208503b6ab875e79c5098eb97d0a1bbf7635
Simon <simon.darmandieu@gmail.com>
2019-01-17 16:53:24 +0100

    file factory, which handles audio video image

--- Projet/back-laravel/database/factories/FileFactory.php
+++ Projet/back-laravel/database/factories/FileFactory.php
@@ -2,11 +2,37 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Image::class, function (Faker $faker) {
-    return [
-        'filename' => $faker->word,
-        'path' => '/images/'.$faker->image('public/storage/images',400,300,null,false),
-        'alt' => $faker->word,
-        'imagetype_id' => rand(1,3)
-    ];
+$factory->define(App\File::class, function (Faker $faker) {
+    $rand = rand(1, 10);
+    switch ($rand) {
+        case(1) :
+            return [
+                'filename' => "audio fake",
+                'path' => '/fake/fake_video.mp4',
+                'extension' => 'mp4',
+                'type' => 'audio',
+                'alt' => "applause",
+                'imagetype_id' => null
+            ];
+            break;
+        case(2) :
+            return [
+                'filename' => "video fake",
+                'path' => '/fake/fake_video.mp4',
+                'extension' => 'mp4',
+                'type' => 'video',
+                'alt' => "talking lion",
+                'imagetype_id' => null
+            ];
+            break;
+        default :
+            return [
+                'filename' => $faker->word,
+                'path' => '/files/image/' . $faker->image('public/storage/files/image', 400, 300, null, false),
+                'extension' => 'png',
+                'type' => 'image',
+                'alt' => $faker->word,
+                'imagetype_id' => rand(1, 3)
+            ];
+    }
 });
