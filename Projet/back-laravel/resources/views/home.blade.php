b829b83ffc680656bbd82a5f482684a1c93f45f3
Simon <simon.darmandieu@gmail.com>
2019-01-18 11:47:14 +0100

    home breadcrumb

--- Projet/back-laravel/resources/views/home.blade.php
+++ Projet/back-laravel/resources/views/home.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.app')
 
+@section('breadcrumb')
+    {{ Breadcrumbs::render('home') }}
+@endsection
+
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div>Blablabla accueil</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div>Blablabla accueil</div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
