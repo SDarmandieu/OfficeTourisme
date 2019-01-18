b25c145e35307b39d1ef86d35096326d767f792e
Simon <simon.darmandieu@gmail.com>
2019-01-18 11:43:00 +0100

    breadcrumb package setup

--- Projet/back-laravel/config/app.php
+++ Projet/back-laravel/config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Barryvdh\Cors\ServiceProvider::class,
+        DaveJamesMiller\Breadcrumbs\BreadcrumbsServiceProvider::class,
 
         /*
          * Package Service Providers...
