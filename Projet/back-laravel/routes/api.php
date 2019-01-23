113ef184619224266e8a3d85464ba6b27b4a4446
Simon <simon.darmandieu@gmail.com>
2019-01-23 15:51:20 +0100

    route to collect data , with resources system

--- Projet/back-laravel/routes/api.php
+++ Projet/back-laravel/routes/api.php
@@ -2,6 +2,17 @@
 
 use Illuminate\Http\Request;
 
+use App\City;
+use App\Http\Resources\City as CityResource;
+use App\Game;
+use App\Http\Resources\Game as GameResource;
+use App\Point;
+use App\Http\Resources\Point as PointResource;
+use App\Question;
+use App\Http\Resources\Question as QuestionResource;
+use App\Answer;
+use App\Http\Resources\Answer as AnswerResource;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,5 +28,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/city','CityController@index');
+Route::get('/database', function () {
+    return [
+        'cities' => CityResource::collection(City::all()),
+        'games' => GameResource::collection(Game::all()),
+        'points' => PointResource::collection(Point::all()),
+        'questions' => QuestionResource::collection(Question::all()),
+        'answers' => AnswerResource::collection(Answer::all()),
+    ];
+});
 
