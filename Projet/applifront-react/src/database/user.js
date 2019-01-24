027310a2f09506d8e0aa8839e99543d08d645e6f
Simon <simon.darmandieu@gmail.com>
2019-01-24 16:57:55 +0100

    function addUser and checkUser

---
+++ Projet/applifront-react/src/database/user.js
@@ -0,0 +1,16 @@
+import db from './db'
+
+export const checkUser = async () => {
+    let user = await db.user.get(1)
+    if (user) return user.name
+}
+
+export const addUser = pseudo => {
+    db.user.put({
+        'id': 1,
+        'name': pseudo,
+        'expe': 0,
+        'questions_done': [],
+        'games_done': []
+    })
+}
