b6b9a7469f6ca455e4c2bdd3fc0079943f245b0f
Simon <simon.darmandieu@gmail.com>
2019-01-24 16:57:23 +0100

    refactor App.js

--- Projet/applifront-react/src/App.js
+++ Projet/applifront-react/src/App.js
@@ -1,69 +1,25 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+import Navigation from './components/Navigation'
+import Routing from './components/Routing'
 import './App.css';
 
-import db from './db';
+import {populate} from './database/populateDB'
 
 class App extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            "city": null
-        }
+        this.state = {}
     }
 
     componentDidMount() {
-        fetch('http://192.168.43.44:8000/api/database')
-            .then(response => response.json())
-            .then(data => {
-                console.log("data", data)
-
-                db.on("ready", function () {
-                    Object.entries(data).forEach(([tableName, tableDatas]) => {
-                        tableDatas.map(current => db.table(tableName).put(current))
-                    })
-                });
-                db.open();
-
-                // const getGame = async () => {
-                //     let game = await db.games.where('id').equals(1).toArray();
-                //     console.log(game)
-                //
-                //     let test = await db.points.where('id').anyOf(game[0]['points']).toArray();
-                //     console.log(test)
-                // }
-                //
-                // getGame()
-                // getGame().then(data => getPoints(data))
-                /*Object.entries(data).forEach(([tableName, tableDatas]) => {
-                    tableDatas.map(current => db.table(tableName)
-                        .add(current)
-                        .then(id => {
-                            const newpoi = [...this.state.markersDexie, Object.assign({}, current, {id})]
-                            this.setState({markersDexie: newpoi})
-                            console.log("marker Dexie", this.state.markersDexie)
-                        }))
-                })*/
-            })
+        populate()
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                </header>
+                <Navigation/>
+                <Routing/>
             </div>
         );
     }
