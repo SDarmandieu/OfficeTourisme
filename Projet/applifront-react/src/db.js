52434ca7b229b16a112dd25715bb033bed684b54
Simon <simon.darmandieu@gmail.com>
2019-01-24 11:26:38 +0100

    add file and its references in dexie DB file

--- Projet/applifront-react/src/db.js
+++ Projet/applifront-react/src/db.js
@@ -3,11 +3,14 @@ import Dexie from 'dexie';
 
 var db = new Dexie("officeTourisme");
 db.version(1).stores({
-    "cities": 'id,name,lat,lon,games',
-    "games": 'id,name,age,desc,city_id',
-    "points" : 'id,lat,lon,desc,city_id',
-    "questions" : "id,content,expe,point_id,game_id",
-    "answers" : "id,content,valid,question_id"
+    "cities": 'id,name,lat,lon',
+    "games": 'id,name,age,desc,city_id,points,files',
+    "points": 'id,lat,lon,desc',
+    "questions": "id,content,expe,point_id,game_id,file_id",
+    "answers": "id,content,valid,question_id,file_id",
+    "files": "id,path,type,extension,alt,imagetype_id",
+    "imagetypes": "id,title",
+    "user": "++id,name,expe,games_done,questions_done"
 });
 
 export default db;
