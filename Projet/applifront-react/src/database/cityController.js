2fb5bae8d952d81c31107ca277112495a2747b59
Simon <simon.darmandieu@gmail.com>
2019-01-25 16:47:38 +0100

    cityIndex & cityShow

--- Projet/applifront-react/src/database/cityController.js
+++ Projet/applifront-react/src/database/cityController.js
@@ -1,4 +1,5 @@
 import db from './db'
 
-export const getCitiesWithGamesInfos = async () => await db.cities.toArray()
+export const cityIndex = async () => await db.cities.toArray()
 
+export const cityShow = async (id) => await db.cities.get(+id)
