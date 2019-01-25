133ac0451857f00879597b7975db1c1e928da4e0
Simon <simon.darmandieu@gmail.com>
2019-01-25 16:48:33 +0100

    fix db.js to match API resources

--- Projet/applifront-react/src/database/db.js
+++ Projet/applifront-react/src/database/db.js
@@ -3,10 +3,10 @@ import Dexie from 'dexie';
 
 let db = new Dexie("officeTourisme");
 db.version(1).stores({
-    "cities": 'id,name,lat,lon',
-    "games": 'id,name,age,desc,city_id,points,files',
+    "cities": 'id,name,lat,lon,games',
+    "games": 'id,name,age,desc,city_id,points,files,done',
     "points": 'id,lat,lon,desc',
-    "questions": "id,content,expe,point_id,game_id,file_id",
+    "questions": "id,content,expe,point_id,game_id,file_id,done",
     "answers": "id,content,valid,question_id,file_id",
     "files": "id,path,type,extension,alt,imagetype_id",
     "imagetypes": "id,title",
