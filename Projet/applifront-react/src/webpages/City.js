6549375e3d043eff031288fcf00edade6e3014ae
Simon <simon.darmandieu@gmail.com>
2019-01-25 16:50:09 +0100

    first draft

---
+++ Projet/applifront-react/src/webpages/City.js
@@ -0,0 +1,37 @@
+import React, {Component} from 'react';
+import {gameIndex} from '../database/gameController'
+import {cityShow} from '../database/cityController'
+import {ListGroup, ListGroupItem} from "react-bootstrap";
+
+export default class City extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            games: [],
+            city: {}
+        }
+    }
+
+    async componentWillMount() {
+        let city = await cityShow(this.props.match.params.id)
+        let games = await gameIndex(this.props.match.params.id)
+        await this.setState({
+            games: games, city: city
+        })
+        console.log('city', this.state.city)
+    }
+
+    render() {
+        let {games, city} = this.state
+        return (
+            <>
+                <h3>Liste des jeux de {city.name}</h3>
+                <ListGroup>
+                    {games.map(game => <ListGroupItem key={game.id}
+                                                      header={game.name}>{game.desc}
+                    </ListGroupItem>)}
+                </ListGroup>
+            </>
+        )
+    }
+}
