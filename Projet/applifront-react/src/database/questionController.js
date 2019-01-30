526818190bd491d2df52ba4cdbbbaa519226f712
Simon <simon.darmandieu@gmail.com>
2019-01-30 14:36:55 +0100

    correct method to match param stucture

--- Projet/applifront-react/src/database/questionController.js
+++ Projet/applifront-react/src/database/questionController.js
@@ -2,10 +2,10 @@ import db from './db'
 
 /**
  * return the question of the chosen point of the current game
- * @param marker_id
+ * @param marker
  * @returns {Dexie.Promise<T>}
  */
-export const questionShow = async marker_id => {
-    let questions = await db.questions.where('point_id').equals(+marker_id[0]).toArray()
-    return questions.find(q => +q.game_id === +marker_id[1])
+export const questionShow = async marker => {
+    let questions = await db.questions.where('point_id').equals(+marker.point.id).toArray()
+    return questions.find(q => +q.game_id === +marker.game.id)
 }
