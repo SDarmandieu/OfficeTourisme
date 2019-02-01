a5233870fe5083741f96ba958c2055e14bb62542
Simon <simon.darmandieu@gmail.com>
2019-02-01 16:57:18 +0100

    add question array to game

--- Projet/applifront-react/src/database/db.js
+++ Projet/applifront-react/src/database/db.js
@@ -4,9 +4,9 @@ import Dexie from 'dexie';
 let db = new Dexie("officeTourisme");
 db.version(1).stores({
     "cities": 'id,name,lat,lon,games',
-    "games": 'id,name,age,desc,city_id,points,files,done',
+    "games": 'id,name,age,desc,city_id,points,files,questions',
     "points": 'id,lat,lon,desc',
-    "questions": "id,content,expe,point_id,game_id,file_id,done",
+    "questions": "id,content,expe,point_id,game_id,file_id",
     "answers": "id,content,valid,question_id,file_id",
     "files": "id,path,type,extension,alt,imagetype_id",
     "imagetypes": "id,title",
