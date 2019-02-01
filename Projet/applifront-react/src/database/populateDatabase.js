d440704d8b46c9d909ac718bd35c3f316fca136a
Simon <simon.darmandieu@gmail.com>
2019-02-01 16:58:20 +0100

    minor

--- Projet/applifront-react/src/database/populateDatabase.js
+++ Projet/applifront-react/src/database/populateDatabase.js
@@ -15,27 +15,27 @@ export const populate = async () => {
     });
     db.open();
 
-            //
-            // const getGame = async () => {
-            //     let game = await db.games.where('id').equals(1).toArray();
-            //     console.log(game)
-            //
-            //     let test = await db.points.where('id').anyOf(game[0]['points']).toArray();
-            //     console.log(test)
-            // }
-            //
-            // getGame()
-            // getGame().then(data => getPoints(data))
+    //
+    // const getGame = async () => {
+    //     let game = await db.games.where('id').equals(1).toArray();
+    //     console.log(game)
+    //
+    //     let test = await db.points.where('id').anyOf(game[0]['points']).toArray();
+    //     console.log(test)
+    // }
+    //
+    // getGame()
+    // getGame().then(data => getPoints(data))
 
 
-            /*Object.entries(data).forEach(([tableName, tableDatas]) => {
-                tableDatas.map(current => db.table(tableName)
-                    .add(current)
-                    .then(id => {
-                        const newpoi = [...this.state.markersDexie, Object.assign({}, current, {id})]
-                        this.setState({markersDexie: newpoi})
-                        console.log("marker Dexie", this.state.markersDexie)
-                    }))
-            })*/
-        // })
+    /*Object.entries(data).forEach(([tableName, tableDatas]) => {
+        tableDatas.map(current => db.table(tableName)
+            .add(current)
+            .then(id => {
+                const newpoi = [...this.state.markersDexie, Object.assign({}, current, {id})]
+                this.setState({markersDexie: newpoi})
+                console.log("marker Dexie", this.state.markersDexie)
+            }))
+    })*/
+    // })
 }
