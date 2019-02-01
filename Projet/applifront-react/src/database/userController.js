ce991e2677e4e5a0380bd600687d68dce0fad5ff
Simon <simon.darmandieu@gmail.com>
2019-02-01 16:59:18 +0100

    fix user controller

--- Projet/applifront-react/src/database/userController.js
+++ Projet/applifront-react/src/database/userController.js
@@ -1,26 +1,27 @@
 import db from './db'
 
-export const checkUser = async () => {
-    let user = await db.user.get(1)
-    if (user) return user.name
-}
+export const checkUser = async () => await db.user.toArray()
 
 export const userStore = pseudo => {
     db.user.put({
         'id': 1,
         'name': pseudo,
         'expe': 0,
-        'questions_done': [],
-        'games_done': []
+        'questions_done': [-1],
+        'games_done': [-1]
     })
 }
 
 /**
- * add expe to user when right answer is provided
- * @param expe
+ * add expe & question done to user when right answer is provided
+ * @param question
  * @returns {Promise<void>}
  */
-export const userGainExpe = async (expe) => {
+export const userQuestionDone = async question => {
     let user = await db.user.get(1)
-    await db.user.update(1, {expe: user.expe + expe})
+    console.log(user.questions_done, typeof user.questions_done)
+    return await db.user.update(1, {
+        expe: user.expe + question.expe,
+        questions_done: [...user.questions_done, question.id]
+    })
 }
