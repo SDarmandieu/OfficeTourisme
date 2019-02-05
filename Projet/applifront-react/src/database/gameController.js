d12b1f9056217fecc5e8037f3a7361f1eaf28a7d
Simon <simon.darmandieu@gmail.com>
2019-02-05 16:50:42 +0100

    gameSearch method to retrieve array of game by game_id

--- Projet/applifront-react/src/database/gameController.js
+++ Projet/applifront-react/src/database/gameController.js
@@ -6,3 +6,10 @@ import db from './db'
  * @returns {Dexie.Promise<Array<T>>}
  */
 export const gameCityIndex = async(city_id)=> await db.games.where('city_id').equals(+city_id).toArray()
+
+/**
+ * return games from a list of game id
+ * @param array_of_game_id
+ * @returns {Dexie.Promise<Array<T>>}
+ */
+export const gameSearch = async (array_of_game_id) => await db.games.where('id').anyOf(array_of_game_id).toArray()
