107711f67f803a02d1414239b22b44f2a8a77b38
Simon <simon.darmandieu@gmail.com>
2019-02-05 16:50:04 +0100

    add comments

--- Projet/applifront-react/src/database/answerController.js
+++ Projet/applifront-react/src/database/answerController.js
@@ -1,3 +1,8 @@
 import db from './db'
 
+/**
+ * get all answers of a specific question
+ * @param question_id
+ * @returns {Dexie.Promise<Array<T>>}
+ */
 export const answerIndex = async (question_id) => await db.answers.where('question_id').equals(+question_id).toArray()
