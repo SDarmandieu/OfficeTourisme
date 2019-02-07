1c7c5ed6d5a84ee83e6e85827ee06d5fc31de33d
Simon <simon.darmandieu@gmail.com>
2019-02-07 15:04:55 +0100

    remove comments

--- Projet/applifront-react/src/database/populateDatabase.js
+++ Projet/applifront-react/src/database/populateDatabase.js
@@ -5,7 +5,7 @@ import db from "./db";
  * @returns {Promise<void>}
  */
 export const populate = async () => {
-    let response = await fetch(`http://192.168.43.44:8000/api/database`)
+    let response = await fetch("http://192.168.43.44:8000/api/database")
     let data = await response.json()
 
     db.on("ready", function () {
@@ -14,28 +14,4 @@ export const populate = async () => {
         })
     });
     db.open();
-
-    //
-    // const getGame = async () => {
-    //     let game = await db.games.where('id').equals(1).toArray();
-    //     console.log(game)
-    //
-    //     let test = await db.points.where('id').anyOf(game[0]['points']).toArray();
-    //     console.log(test)
-    // }
-    //
-    // getGame()
-    // getGame().then(data => getPoints(data))
-
-
-    /*Object.entries(data).forEach(([tableName, tableDatas]) => {
-        tableDatas.map(current => db.table(tableName)
-            .add(current)
-            .then(id => {
-                const newpoi = [...this.state.markersDexie, Object.assign({}, current, {id})]
-                this.setState({markersDexie: newpoi})
-                console.log("marker Dexie", this.state.markersDexie)
-            }))
-    })*/
-    // })
 }
