3e383f84b0ea6db706b5841bab83bb265c43cc12
Simon <simon.darmandieu@gmail.com>
2019-02-14 11:56:00 +0100

    add default value false to field published for games table

--- Projet/back-laravel/database/migrations/2018_12_17_143647_create_games_table.php
+++ Projet/back-laravel/database/migrations/2018_12_17_143647_create_games_table.php
@@ -18,7 +18,7 @@ class CreateGamesTable extends Migration
             $table->string('name');
             $table->string('age');
             $table->text('desc');
-            $table->boolean('published');
+            $table->boolean('published')->default(false);
             $table->integer('city_id')->unsigned();
             $table->timestamps();
 
