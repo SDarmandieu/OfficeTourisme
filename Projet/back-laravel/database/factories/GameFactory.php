56da0182389594a922f1843e46ae50ff3001a9ff
Simon <simon.darmandieu@gmail.com>
2019-02-14 11:56:29 +0100

    randomize published field for game factory

--- Projet/back-laravel/database/factories/GameFactory.php
+++ Projet/back-laravel/database/factories/GameFactory.php
@@ -4,10 +4,11 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Game::class, function (Faker $faker) {
     $ages = ['7/9 ans','9/11 ans','11/13 ans'];
+    $rand = rand(0,1);
     return [
         'name' => $faker->word,
         'age' => $ages[array_rand($ages)],
         'desc' => $faker->word,
-        'published' => false
+        'published' => [true,false][$rand]
     ];
 });
