f1f084c5c06f1655b51be6015d29488c95263004
Simon <simon.darmandieu@gmail.com>
2019-02-18 09:27:52 +0100

    minor

--- Projet/back-laravel/resources/views/file/create.blade.php
+++ Projet/back-laravel/resources/views/file/create.blade.php
@@ -5,7 +5,6 @@
 @endsection
 
 @section('content')
-
     <div class="container">
         <div class="card">
             <div class="card-header">Création de fichier pour {{$city->name}}</div>
@@ -75,9 +74,7 @@
                         let imageTypeInput = `<div class="form-group row">
                         <label for="imagetype" class="col-md-4 col-form-label text-md-right">Type d'image</label>
                         <select name="imagetype" class="form-control col-md-6" id="imagetype" required>
-
                             <option selected disabled hidden>Choisissez un type d'image</option>
-
                             @foreach($imagetypes as $imagetype)
                             <option value="{{$imagetype->id}}">{{$imagetype->title}}</option>
                             @endforeach
@@ -85,7 +82,6 @@
                         </div>`
                         $('#ifImage').html(imageTypeInput)
                     } else $('#ifImage').empty()
-
                     if ($('#file')[0].files[0]['size'] > 15000000) {
                         $('.text-danger').removeAttr('hidden')
                     } else $('.text-danger').attr('hidden', 'hidden')
